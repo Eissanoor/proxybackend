531e279018ba1ae3bcadb111f3bcfb75b20e7eec
Eissanoor <eissaanoor@gmail.com>
2025-02-14 22:58:30 +0500

    Update welcome message in app.js to specify API context

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use("/api/v1", routes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/",(req,res)=>{
-  res.send("Welcome to the chat app");
+  res.send("Welcome to the chat app API");
 });
 
 // Error Routes
