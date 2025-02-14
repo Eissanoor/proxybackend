d72d1fb55b217f762522fe208c4f7fc5d269da42
Eissanoor <eissaanoor@gmail.com>
2025-02-14 21:41:48 +0500

    Update welcome message in app.js

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.use("/api/v1", routes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/",(req,res)=>{
-  res.send("Welcomeaaaaaa");
+  res.send("Welcome");
 });
 
 // Error Routes
